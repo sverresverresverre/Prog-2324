2726ebd69e173684819d9b66648f2614b79ced44
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2023-08-31 10:30:57 +0200

    jhwdfjgh

---
+++ Hello/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(" Hello, Chompi");
+Console.WriteLine(" freeeeeze");
+
+string name = "mickeB";
+
+Console.WriteLine($" tja {name} alltid taggad för freeze");
+
+Console.ReadLine();
